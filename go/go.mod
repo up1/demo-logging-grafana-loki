9f3231aa7642e6de4648c9ea64a8dc80d3c47dc8
up1 <somkiat.p@gmail.com>
2023-10-09 22:51:28 +0700

    Add demo with go

--- go/go.mod
+++ go/go.mod
@@ -2,6 +2,19 @@ module demo
 
 go 1.21.1
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/sirupsen/logrus v1.9.3
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0
+	go.opentelemetry.io/otel v1.19.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.19.0
+	go.opentelemetry.io/otel/sdk v1.19.0
+	go.opentelemetry.io/otel/trace v1.19.0
+)
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.19.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- go/server.go
+++ go/server.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
-	"math/rand"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
-	logger := Logger()
-	for {
-		switch i := rand.Intn(4); i {
-		case 0:
-			logger.WithFields(logrus.Fields{"file": "main.go"}).Debug("this is a DEBUG message")
-		case 1:
-			logger.WithFields(logrus.Fields{"file": "main.go"}).Error("this is an ERROR message")
-		case 2:
-			logger.WithFields(logrus.Fields{"file": "main.go"}).Info("this is an INFO message")
-		case 3:
-			logger.WithFields(logrus.Fields{"file": "main.go"}).Warn("this is an WARN message")
-		default:
-			logger.WithFields(logrus.Fields{"file": "main.go"}).Debug("this is a DEBUG message")
+
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
 		}
-		time.Sleep(1 * time.Second)
+	}()
+
+	logger := Logger()
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		// Get current trace
+		ctx := req.Context()
+		span := trace.SpanFromContext(ctx)
+		traceId := span.SpanContext().TraceID().String()
+		spanId := span.SpanContext().SpanID().String()
+
+		// Write log
+		logs := logrus.Fields{"file": "main.go", "service": "go-service", "trace_id": traceId, "span_id": spanId}
+		logger.WithFields(logs).Debug("this is a DEBUG message")
+		logger.WithFields(logs).Error("this is an ERROR message")
+		logger.WithFields(logs).Info("this is an INFO message")
+
+		_, _ = io.WriteString(w, "Hello, world!\n")
+	}
+
+	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
+	http.Handle("/", otelHandler)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -36,4 +62,24 @@ func Logger() *logrus.Logger {
 	log.SetOutput(logger.Writer())
 	logger.SetOutput(io.MultiWriter(os.Stdout))
 	return logger
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	// Create stdout exporter to be able to retrieve
+	// the collected spans.
+	_, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		return nil, err
+	}
+
+	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
+	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		// sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceName("go-service"))),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, err
 }
